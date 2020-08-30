9002e8e3b37145e7ded585c00aaf8b19930e6db9
Otávio <otav-o@users.noreply.github.com>
2020-08-30 17:46:46 -0300

    exercicios

---
+++ ExercíciosAulaCamillo/Exercicio1.cs
@@ -0,0 +1,13 @@
+/*
+1.Faça um programa que lê uma temperatura em graus Celsius e apresenta-a convertida em graus Fahrenheit. A fórmula de conversão: F ← (9*C+160)/5
+*/
+
+Using System;
+namespace Exercicio1 {
+class Exercicio1 {
+	public static void Main(string[] args) {
+		Console.Write("Digite uma temperatura em °C: ");
+		double tempCelsius = Convert.ToDouble(Console.ReadLine());
+
+	}
+}}
