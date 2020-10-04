856e2ee759cc2134189abf469a2d898c58562d5b
Otávio <otav-o@users.noreply.github.com>
2020-10-04 16:22:43 -0300

    até aula 36
    obs.: aula 36 não finalizada

---
+++ CFB/Aula036-Protected.cs
@@ -0,0 +1,64 @@
+using System;
+class Doce {	// base
+	public int acucar;
+	private int sodio;	// restringe à classe
+	protected bool saudavel;
+	
+	public Doce (int sodio) {
+		acucar = 0;
+		this.sodio = sodio;
+        	saudavel = false;
+	}
+
+    	public bool getSaudavel() {
+        	return saudavel;
+    	}
+
+    	public int getSodio() {
+        	return sodio;
+    	}
+}
+
+class DoceFesta:Doce { // derivada de veículo
+    	public string nome;
+    	public DoceFesta(string nome, int s):base(int s) { 	// o valor será passado para o Doce(int sodio)
+
+        	this.nome = nome;
+   		// sodio = s não pode (private)
+        
+        	saudavel = true; // pode pois é classe derivada (pode falar "sub-classe?")
+   
+    	}
+}
+class Aula36 {
+    	static void Main() {
+        	Doce d1 = new Doce(40);
+	        DoceFesta d2 = new DoceFesta("Goiabada", 30);
+        
+
+        	Console.WriteLine(d1.acucar); // 0
+        
+        	// Console.WriteLine(d1.sodio);  // erro; sodio é privado
+
+        	Console.WriteLine(d1.getSodio()); // 40
+        
+        	// Console.WriteLine(d1.nome); // não tem
+        
+        	// Console.WriteLine(d1.saudavel); // erro; é protected
+
+        	Console.WriteLine(d1.getSaudavel()); // false
+        
+		Console.WriteLine("-=-=-=-=-=-=");
+
+        	Console.WriteLine(d2.acucar); // 0
+        	// Console.WriteLine(d2.sodio); // erro; sodio é privado
+        	Console.WriteLine(d2.getSodio); // 30
+        	Console.WriteLine(d2.nome); // Goiabada
+        
+        	// Console.WriteLine(d2.saudavel); // erro, é protected
+        
+        	Console.WriteLine(d2.getSaudavel) // true
+        
+        
+    	}
+}
