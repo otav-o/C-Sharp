8040f07bb6a77783e9e919fd1f4aa4a9e319d3b7
otav-o <otaviocacadordioscanio@gmail.com>
2021-01-18 20:19:22 -0300

    Testando generics início

---
+++ Treinamento Estágio Manager Saúde/Generics/Generics/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Generics
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Start();
+        }
+
+        private static void Start()
+        {
+            int[] intArray = CreateArray(5, 6);
+            Console.WriteLine("Tipo: " + intArray.GetType() + "\nTamanho: " + intArray.Length + "\nPosição 1: " + intArray[0] + "\nPosição 2: " + intArray[1]);
+
+        }
+
+        private static int[] CreateArray(int firstElement, int secondElement)
+        {
+            return new int[] { firstElement, secondElement };
+        }
+    }
+}
