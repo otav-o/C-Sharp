2a61687bac68e191d3268643b4e39622d10c2a0f
otav-o <otaviocacadordioscanio@gmail.com>
2021-01-19 18:02:06 -0300

    Commit inicial Delegates

---
+++ Treinamento Estágio Manager Saúde/Delegates/Delegates/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Delegates
+{
+    class Program
+    {
+        public delegate void TestDelegate(); // define um tipo de delegate
+        private TestDelegate testDelegateFunction;
+        static void Main(string[] args)
+        {
+            var p1 = new Program();
+            p1.testDelegateFunction = p1.MyTestDelegateFunction;
+            // atribui a função ao delegate. Ela fica guardada lá e pode ser acessada pelo nome adicionado de parâmetros
+            p1.testDelegateFunction();
+        }
+        private void MyTestDelegateFunction() // declação de função que condiz com o delegate
+        {
+            Console.WriteLine("Testing MyTestDelegateFunction");
+        }
+    }
+}
