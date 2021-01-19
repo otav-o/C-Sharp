96c2bd4ff42d2dd2e7acddc04a49860317f27a5b
otav-o <otaviocacadordioscanio@gmail.com>
2021-01-19 18:32:18 -0300

    Função anônima e Lambda

--- Treinamento Estágio Manager Saúde/Delegates/Delegates/Program.cs
+++ Treinamento Estágio Manager Saúde/Delegates/Delegates/Program.cs
@@ -12,13 +12,20 @@ namespace Delegates
         static void Main(string[] args)
         {
             var p1 = new Program();
-            p1.testDelegateFunction = p1.MyTestDelegateFunction;
-            // no fundo é isso que acontece: testDelegateFunction = new TestDelegate(MyTestDelegateFunction);
+            p1.testDelegateFunction = delegate () { Console.WriteLine("Anonymous method"); };
+            // método anônimo deve seguir a assinatura do delegate (inclusive os parâmetros)
 
             p1.testDelegateFunction();
 
-            p1.testBoolDelegateFunction = p1.MyTestBoolDelegateFunction;
-            Console.WriteLine(p1.testBoolDelegateFunction(10));
+            p1.testDelegateFunction = () => { Console.WriteLine("Lambda expression"); };
+
+            p1.testDelegateFunction();
+
+            p1.testBoolDelegateFunction = (int i) => { return i < 5; };
+            p1.testBoolDelegateFunction = (int i) => i < 5; 
+                // modo + compacto se a função tiver apenas uma linha
+
+            Console.WriteLine(p1.testBoolDelegateFunction(4));
         }
         private void MyTestDelegateFunction() // declação de função que condiz com o delegate
         {
